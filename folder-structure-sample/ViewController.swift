572ca637397a3b7420c864511c0b2e5f8b7cde28
yhonda <yhonda@opendoor.co.jp>
2019-11-01 12:37:31 +0900

    Initial Commit

---
+++ folder-structure-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  folder-structure-sample
+//
+//  Created by yhonda on 2019/11/01.
+//  Copyright © 2019 yhonda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ folder-structure-sampleTests/folder_structure_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  folder_structure_sampleTests.swift
+//  folder-structure-sampleTests
+//
+//  Created by yhonda on 2019/11/01.
+//  Copyright © 2019 yhonda. All rights reserved.
+//
+
+import XCTest
+@testable import folder_structure_sample
+
+class folder_structure_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
