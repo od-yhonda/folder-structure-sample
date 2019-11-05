fb7a3cac1944c1f6c25252c390367d68d58a6817
yhonda <yhonda@opendoor.co.jp>
2019-11-05 10:26:04 +0900

    sample

---
+++ folder-structure-sample/Models/DB/Table1/Table1Dao.swift
@@ -0,0 +1,9 @@
+//
+//  DA.swift
+//  folder-structure-sample
+//
+//  Created by yhonda on 2019/11/01.
+//  Copyright © 2019 yhonda. All rights reserved.
+//
+
+import Foundation
---
+++ folder-structure-sample/Utils/SampleConst.swift
@@ -0,0 +1,9 @@
+//
+//  SampleConst.swift
+//  folder-structure-sample
+//
+//  Created by yhonda on 2019/11/05.
+//  Copyright © 2019 yhonda. All rights reserved.
+//
+
+import Foundation
